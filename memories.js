92d5657fecf259c1e793c0a888b1e9517afb5015
caohieu-281 <kvlingxss@gmail.com>
2026-06-10 02:15:16 +0900

    Love love love

---
+++ memories.js
@@ -0,0 +1,16 @@
+window.MEMORIES = [
+  { img: 'images/att.att.IsdDneG6o1CpBgvuARXpLAA_N0AXhoO2Fee86JONsyk.JPG', msg: 'Kỉ niệm 1 năm iu nhau' },
+  { img: 'images/IMG_0224.JPG', msg: 'Lúc còn xa, đang tán tỉnh tán tỉnh các thứ' },
+  { img: 'images/FB3EF077-170E-4CBD-B2B3-DC0619376FAA.JPG', msg: 'Lúc anh ngỏ lời và được đồng ý 💕' },
+  { img: 'images/IMG_1728.JPG', msg: 'Dạm ngõ - đẹp mặt 2 anh chị chưa' },
+  { img: 'images/IMG_6146 2.PNG', msg: 'First date - chưa phải ny, ngại ngùng điêng' },
+  { img: 'images/IMG_9199.jpg', msg: 'Người đẹp sang Nhật vs a - dạo chơi disneysea ' },
+  { img: 'images/IMG_8611.jpg', msg: 'Lần đầu đi chơi xa với nhau' },
+  { img: 'images/IMG_9320.jpg', msg: 'Ngắm pháo hoa cùng người đẹp' },
+
+  { video: 'videos/att._tNfxorUkGDKEk-H6ijhaooNjgH0RqkFGuMkuNzsJR4.MP4',   msg: 'Khoảnh khắc em đồng ý 💍' },
+  { video: 'videos/att.hDJwjy6WvXHwR7sAlToyfb1dlUw1Slrsd_3LZzVPPmc.MP4', msg: 'Dạo Tết cùng vại tương lơ 🎡' },
+  { video: 'videos/77bac798d723433f9c26621914151683.MOV', msg: 'Điện ảnh chút thuiiii' },
+  { video: 'videos/IMG_2287.MOV', msg: 'Emiu ngã xe, thương vcđiênguc' },
+  { video: 'videos/IMG_9340.MOV', msg: 'Chạy sml ở Shibuya, zuiiii' },
+];
